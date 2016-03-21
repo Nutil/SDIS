34a3c69da8b34aa7676681872fc54b4b98cd5287
Digas29 <trindadeguarda@gmail.com>
2016-03-21 17:29:35 +0000

    Message done

--- src/Header.java
+++ src/Header.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 /**
  * Created by Diogo Guarda on 16/03/2016.
  */
@@ -5,6 +8,7 @@ public class Header {
 
     public static final byte CR = 0xD;
     public static final byte LF = 0xA;
+    private static final String[] types = {"PUTCHUNK","STORED", "GETCHUNK", "CHUNK", "DELETE", "REMOVED"};
     private String messageType;
     private String version;
     private int senderId;
@@ -12,7 +16,7 @@ public class Header {
     private int chunkNo;
     private int replicationDegree;
 
-    public Header(String message){
+    public Header(String message) throws  IllegalArgumentException{
         String[] fields = message.split(" ");
         messageType = fields[0];
         version = fields[1];
@@ -20,8 +24,32 @@ public class Header {
         fileId = fields[3];
         chunkNo = Integer.parseInt(fields[4]);
         replicationDegree = Integer.parseInt(fields[5]);
+
+        if(!Arrays.asList(types).contains(messageType))
+            throw new IllegalArgumentException("Invalid <MessageType>");
+
+        Pattern r = Pattern.compile("[0-9]*.[0-9]");
+        if(!r.matcher(version).matches())
+            throw new IllegalArgumentException("Invalid <Version>");
+
     }
 
+    public Header(String messageType, String version, int senderId, String fileId, int chunkNo, int replicationDegree)
+            throws IllegalArgumentException{
+        if(!Arrays.asList(types).contains(messageType))
+            throw new IllegalArgumentException("Invalid <MessageType>");
+        Pattern r = Pattern.compile("[0-9]*.[0-9]");
+        if(!r.matcher(version).matches())
+            throw new IllegalArgumentException("Invalid <Version>");
+
+        this.messageType = messageType;
+        this.version = version;
+        this.senderId = senderId;
+        this.fileId = fileId;
+        this.chunkNo = chunkNo;
+        this.replicationDegree = replicationDegree;
+
+    }
     public String getMessageType() {
         return messageType;
     }
